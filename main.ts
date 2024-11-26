964905f238d6b52184aa429208f703c56df05b4c
irmaDulce <letramentodigital4b@gmail.com>
2024-11-26 15:32:42 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(input.lightLevel())
+})
+basic.showIcon(IconNames.Happy)
